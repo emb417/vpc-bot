b17503da494c7de78c241c3f1acf0d855c1a37a9
Eric Brousseau <emb417@gmail.com>
2026-02-23 17:07:52 -0800

    improved ticket breakdown visibility on raffle board

--- src/commands/raffle/show-raffle-board.js
+++ src/commands/raffle/show-raffle-board.js
@@ -63,7 +63,7 @@ export const showRaffleBoard = async (interaction) => {
         const tableUrl = user.table.url;
         const tableName = user.table.name;
         const tableLink = tableUrl ? `[${tableName}](${tableUrl})` : tableName;
-        return `${user.tickets} 🎟 (${user.probability.toFixed(1)}%) — #${user.rank} — ${user.username} — ${tableLink}`;
+        return `${user.tickets} 🎟 (${user.probability.toFixed(1)}%)  — #${user.rank}. ${user.username} (${user.performanceTickets} 🏆 ${user.persistenceTickets} 🔥) — ${tableLink}`;
       })
       .join("\n");
 
